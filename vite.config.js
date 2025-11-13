8f5fec7ecb68cc7e8d4540e838c5f78e9d741034
abolfazl <a.khakzad1993@gmail.com>
2025-11-13 22:30:15 +0100

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import prerender from "vite-plugin-prerender"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      routes: [
-        "/", 
-        "/create", 
-        "/discover"
-      ]
-    })
-  ],
+  plugins: [react()],
   base: "/", // ✅ مسیر پایه برای هاست‌های استاتیک (خیلی مهم برای Vercel/Netlify)
   build: {
     outDir: "dist", // پیش‌فرض خودش هست ولی شفاف‌تره
